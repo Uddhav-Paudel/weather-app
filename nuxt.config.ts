83f8725405fd9dcb985abdcabcf47edd32e23460
Uddhav Paudel <devuddhav15@gmail.com>
2024-06-21 14:32:10 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr:true,
+  bridge: {
+    nitro: false,
+  },
   devServer: {
     https: {
       key:'./localhost-key.pem',
